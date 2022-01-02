f5c0c832e530108abb85f20cc342a0e396b2c332
seunga <osa0215@naver.com>
2022-01-02 22:50:25 +0900

    [BOJ] 2579 - python

--- DP/2579_계단오르기.py
+++ DP/2579_계단오르기.py
@@ -1,6 +1,17 @@
 N = int(input())
-d=[0]*(N+1)
+stair=[0]*(N+1)
 
-for i in range(N):
-    d[i] = int(input())
+for i in range(1,N+1):
+    stair[i] = int(input())
 
+if N == 1:
+    print(stair[1])
+else:
+    dp = [0] * (N+1)
+    dp[1] = stair[1]
+    dp[2] = stair[1] + stair[2] 
+
+    for i in range(3, N+1):
+        dp[i] = max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i])  
+
+print(dp[N])
