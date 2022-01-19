240a9fb8258069841af61f20ba100cb704c99efb
seunga <osa0215@naver.com>
2022-01-19 23:16:04 +0900

    2231 ing

--- 브루트 포스/2231_분해합.py
+++ 브루트 포스/2231_분해합.py
@@ -6,12 +6,14 @@ m = l * 9
 r=0
 minlist = []
 
-for i in range(N-m, N):
+# for i in range(N-m, N):
+for i in range(1, N):
+    # print(i)
     for j in str(i):
         r += int(j)
 
     if int(i)+r == N:
-        minlist.append(i)
+        print(i)        #i가 작은 수 부터 들어가기 때문에 생성자를 찾으면 중단해도 됨.
+        break
     r = 0
-
-print(min(minlist))
+print(0)
