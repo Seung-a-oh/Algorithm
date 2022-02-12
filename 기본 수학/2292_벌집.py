0bb09c76aae2f0de01fb5edd60fce4b8e22a6402
seunga <osa0215@naver.com>
2022-02-12 21:55:54 +0900

    [BOJ] 2292 - python

--- 기본 수학/2292_벌집.py
+++ 기본 수학/2292_벌집.py
@@ -1,12 +1,8 @@
-import sys
-N = int(sys.stdin.readline().rstrip())
+N = int(input())
 
-num = 1
 count = 1
-while(N>num):
-    num = num + 6
+while N > 1:
+    N -= (6 * count)
     count += 1
 
 print(count)
-
-# 시간 초과
