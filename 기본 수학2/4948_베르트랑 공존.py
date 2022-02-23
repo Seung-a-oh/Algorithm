43f4fe7dcaa14e59e84b9e362552c710467d7cb5
seunga <osa0215@naver.com>
2022-02-24 00:05:49 +0900

    [BOJ] 4948 - python

--- 기본 수학2/4948_베르트랑 공존.py
+++ 기본 수학2/4948_베르트랑 공존.py
@@ -18,15 +18,34 @@
 #     print(count)
 # -------------------------------------------
         
+# -----------------또 시간 초과---------------
+# while True:
+#     N = int(input())
+#     if N == 0: break
+    
+#     count = 0
+#     for i in range(N+1, 2*N+1):
+#         for n in range(2, int(i**0.5)+1):
+#             if i % n == 0:
+#                 break
+#         count += 1
+#     print(count)
+# ------------------------------------------
 
+k = 123456*2+1
+all = [1]*k
+
+for i in range(2,k):
+    for j in range(2, int(i**0.5)+1):
+        if i % j == 0:
+            all[i] = 0
+            break
 while True:
     N = int(input())
     if N == 0: break
     
-    count = 0
+    count = 0; sum = 0
+    
     for i in range(N+1, 2*N+1):
-        for n in range(2, int(i**0.5)+1):
-            if i % n == 0:
-                break
-        count += 1
-    print(count)
+        sum += all[i]
+    print(sum)
