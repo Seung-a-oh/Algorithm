4af5a4ddc7c1c7d276eabd8b3407f71635736e07
seunga <osa0215@naver.com>
2022-02-24 23:04:32 +0900

    [BOJ] 입출력과 사칙연산 - python

---
+++ 입출력과 사칙연산/10430_나머지.py
@@ -0,0 +1,7 @@
+A,B,C = map(int, input().split())
+
+print((A+B)%C )
+print(((A%C) + (B%C))%C)
+
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
---
+++ 입출력과 사칙연산/2588_곱셈.py
@@ -0,0 +1,16 @@
+a = input()
+b = input()
+
+print(int(a)*int(b[2]))
+print(int(a)*int(b[1]))
+print(int(a)*int(b[0]))
+print(int(a)*int(b))
+
+# a = input()
+# b = input()
+
+# print(int(a)*int(b[-1]))
+# print(int(a)*int(b[-2]))
+# print(int(a)*int(b[-3]))
+# print(int(a)*int(b))
+# --------> - 로 하는 것이 실행 속도가 더 빠름
