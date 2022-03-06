f80d79d699d160fa5f2a63ba14aba37d39cbacd8
seunga <osa0215@naver.com>
2022-03-06 23:34:36 +0900

    또 다시 메모리 초과...

--- 정렬/2473_세 용액.py
+++ 정렬/2473_세 용액.py
@@ -1,16 +1,27 @@
-from itertools import combinations
-
 n = int(input())
-a = list(map(int, input().split()))
+liquid = list(map(int, input().split()))
 
-combi = list(combinations(a,3))
-b = abs(sum(combi[0]))
-answer = combi[0]
+dict = {}
+for a in liquid:
+    for b in liquid[liquid.index(a)+1:]:
+        for c in liquid[liquid.index(b)+1:]:
+            dict[a,b,c] = abs(a+b+c)
 
-for _ in combi:
-    if b > abs(sum(_)):
-        b = abs(sum(_))
-        answer = list(_)
+min_list = [list(k) for k,v in dict.items() if min(dict.values()) == v]
+min_list = min_list[0]
+min_list.sort()
+print(min_list[0],min_list[1],min_list[2])
+
+# from itertools import combinations
+#
+# combi = list(combinations(liquid,3))
+# b = abs(sum(combi[0]))
+# answer = combi[0]
+
+# for _ in combi:
+#     if b > abs(sum(_)):
+#         b = abs(sum(_))
+#         answer = list(_)
         
-answer.sort()
-print(answer[0],answer[1],answer[2])
+# answer.sort()
+# print(answer[0],answer[1],answer[2])
