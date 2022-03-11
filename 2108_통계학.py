9d0958405ada84eb3d232ac2b278d938b94dcd70
seunga <osa0215@naver.com>
2022-03-12 00:29:12 +0900

    시간 초과

--- 2108_통계학.py
+++ 2108_통계학.py
@@ -1,7 +1,3 @@
-import weakref
-from matplotlib.text import get_rotation
-
-
 N = int(input())
 array = []
 
@@ -10,5 +6,17 @@ for _ in range(N):
 
 print(round(sum(array)/N))
 array.sort(); print(array[N//2])
-print("최빈값")
+
+dict = {}
+for _ in array:
+    dict[_] = array.count(_)
+sorted_dict = sorted(dict.items(),key = lambda item: item[1], reverse=True)
+
+if len(sorted_dict) <= 1:
+    print(array[0])
+elif sorted_dict[0][1] == sorted_dict[1][1]:
+    print(sorted_dict[1][0])
+else:
+    print(sorted_dict[0][0])
+
 print(max(array)-min(array))
