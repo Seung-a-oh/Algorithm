89fd3b403bebfbe0c661bd84e1f22d5c260ca496
seunga <osa0215@naver.com>
2022-03-19 15:50:33 +0900

    주석 추가

--- 1946_신입 사원.py
+++ 1946_신입 사원.py
@@ -5,9 +5,14 @@ for _ in range(T):
     N = int(sys.stdin.readline())
     score = [0]*N
     for i in range(N):
+        # 입력을 공백으로 구분하여 각 값을 리스트로 저장한다.
+        # score 리스트 안에 리스트로 저장된다.
         score[i] = list(map(int,sys.stdin.readline().split()))
+    # 서류 심사 성적을 기준으로 오름차순 정렬한다.
     score.sort()
+    # 면접 심사 순위가 서류 심사 앞 등수의 사람 면접 심사 순위보다 낮다면 탈락이다.
     min = score[0][1]
+    # 서류 심사 1등은 무조건 합격이므로 count를 1로 초기화한다.
     count = 1
     for j in range(1,N):
         if score[j][1] < min:
--- 2108_통계학.py
+++ 2108_통계학.py
@@ -51,7 +51,7 @@
 
 # print(max(array)-min(array))
 
-# 3차 코드 - 성공
+# 3차 코드
 import sys
 from collections import Counter
 
@@ -61,15 +61,24 @@ array = []
 for _ in range(N):
     array.append(int(sys.stdin.readline()))
 
+# 산술 평균
 print(round(sum(array)/N))
 
+# 중앙값
 array.sort()
 print(array[N//2])
 
+# 최빈값
+# array에서 값 별 횟수를 세어서 저장해준다.
+# 횟수를 기준으로 내림차순으로 정렬되고, 동일 횟수에서는 값을 기준으로 오름차순 정렬된다.
 common = Counter(array).most_common()
+# 값이 하나 이상이고, 최빈값이 두 개 이상일 경우
 if len(common) > 1 and common[0][1] == common[1][1]:
+    # 두번째 최빈값의 값을 출력하고
     print(common[1][0])
 else:
+    # 최빈값이 하나일 경우 최빈값의 값을 출력한다.
     print(common[0][0])
 
+# 범위
 print(max(array)-min(array))
