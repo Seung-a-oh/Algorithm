7b413594460dc9dab091b84ed7509bfb0e533ee5
seunga <osa0215@naver.com>
2022-04-25 14:57:44 +0900

    10844 - 시간 초과

--- DP/10844_쉬운 계단 수.py
+++ DP/10844_쉬운 계단 수.py
@@ -1,15 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-all_number = [str(i) for i in range(10**(N-1), 10**N)]
 count = 0
 
 if N == 1:
     print(9)
     sys.exit()
     
-for num in all_number:
+for num in range(10**(N-1), 10**N):
+    s_num = str(num)
     for n in range(0,N-1):
-        if abs(int(num[n])-int(num[n+1])) == 1:
+        if abs(int(s_num[n])-int(s_num[n+1])) == 1:
             count += 1
 print(count)
