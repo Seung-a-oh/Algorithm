7f4044577797e16f77e35e64f522d3f44ae029d8
seunga <osa0215@naver.com>
2022-04-27 22:12:18 +0900

    [BOJ] 10844 - python

--- DP/10844_쉬운 계단 수.py
+++ DP/10844_쉬운 계단 수.py
@@ -14,19 +14,22 @@
 #             count += 1
 # print(count)
 
+import sys
 
-n = int(input())
+N = int(sys.stdin.readline())
 
 dp = [[0 for i in range(10)] for j in range(101)]
-for i in range(1, 10):
-    dp[1][i] = 1
-for i in range(2, n + 1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i - 1][1]
-        elif j == 9:
-            dp[i][j] = dp[i - 1][8]
+
+for k in range(1, 10):
+    dp[1][k] = 1
+
+for n in range(2, N + 1):
+    for k in range(10):
+        if k == 0:
+            dp[n][k] = dp[n-1][1]
+        elif k == 9:
+            dp[n][k] = dp[n-1][8]
         else:
-            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-            
+            dp[n][k] = dp[n-1][k-1] + dp[n-1][k+1]
+
 print(sum(dp[n]) % 1000000000)
