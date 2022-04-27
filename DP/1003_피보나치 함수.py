1f6593e6d2a87b69c718c52eb89f89a0982dc349
seunga <osa0215@naver.com>
2022-04-27 19:14:58 +0900

    [BOJ] 1003 - python

--- DP/1003_피보나치 함수.py
+++ DP/1003_피보나치 함수.py
@@ -3,18 +3,20 @@ import sys
 T = int(sys.stdin.readline())
 
 for _ in range(T):
-    zero = 0
-    one = 0
     N = int(sys.stdin.readline())
-    dp = [[0,0] for i in range(N)]
+    dp = [[0,0] for i in range(N+1)]
+
     if N == 0:
         print("1 0")
         continue
     elif N == 1:
         print("0 1")
         continue
+    
     dp[0] = [1,0]
     dp[1] = [0,1]
-    for j in range(len(dp))[2:]:
+
+    for j in range(N+1)[2:]:
         dp[j] = [dp[j-1][k] + dp[j-2][k] for k in range(2)]
-    print(dp[N-2][0]+dp[N-1][0], dp[N-2][1]+dp[N-1][1])
+
+    print(dp[N][0], dp[N][1])
