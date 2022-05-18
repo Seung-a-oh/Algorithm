d029d3ac0d8db975f4a6177e8ca329a5fbfb2924
seunga <osa0215@naver.com>
2022-05-18 19:24:56 +0900

    [BOJ] 11722 - python

---
+++ DP/11722_가장 긴 감소하는 부분 수열.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = list(map(int, sys.stdin.readline().split()))
+
+dp = [1 for i in range(N)]
+
+for i in range(N):
+    for j in range(i):
+        if arr[i] < arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
