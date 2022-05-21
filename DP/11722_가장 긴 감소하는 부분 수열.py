de6dc9589a37ef6b2c78ac6ae328ca37b7425868
seunga <osa0215@naver.com>
2022-05-21 21:22:00 +0900

    [BOJ] 11722 - 설명 추가

--- DP/11722_가장 긴 감소하는 부분 수열.py
+++ DP/11722_가장 긴 감소하는 부분 수열.py
@@ -1,13 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
+li = list(map(int, sys.stdin.readline().split()))
 
 dp = [1 for i in range(N)]
 
+# 10 30 10 20 20 10
+
 for i in range(N):
     for j in range(i):
-        if arr[i] < arr[j]:
+        if li[i] < li[j]:
             dp[i] = max(dp[i], dp[j]+1)
 
 print(max(dp))
