f9ca409389a3833a7a0896ea3c1f8cddb3ee198c
seunga <osa0215@naver.com>
2022-07-09 00:04:57 +0900

    [Programmers] python

--- Programmers Lv1/완주하지 못한 선수.py
+++ Programmers Lv1/완주하지 못한 선수.py
@@ -1,8 +1,21 @@
+from collections import Counter
+
 def solution(participant, completion):
     answer = ''
-    
-    for i in completion:
-        participant.remove(i)
-    answer = participant[0]
+
+    p = Counter(participant)
+    c = Counter(completion)
+    a = p-c
+    answer = [k for k,v in a.items() if v == 1][0]
 
     return answer
+
+p = ["mislav", "stanko", "mislav", "ana"]
+c = ["mislav", "stanko", "ana"]
+print(solution(p,c))
+
+
+# 다른 사람의 코드
+# def solution(participant, completion):
+#     answer = collections.Counter(participant) - collections.Counter(completion)
+#     return list(answer.keys())[0]
